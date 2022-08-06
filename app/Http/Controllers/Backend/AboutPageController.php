e118da9d668fe4a74fe0085d7d4783cc2db66a9f
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2022-08-06 20:41:57 +0600

    8-6-2022

--- app/Http/Controllers/Backend/AboutPageController.php
+++ app/Http/Controllers/Backend/AboutPageController.php
@@ -32,15 +32,4 @@ class AboutPageController extends Controller
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
---
+++ database/migrations/2022_08_03_200106_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('image');
+            $table->string('title', 80);
+            $table->string('writer_logo');
+            $table->string('writer_name', 20);
+            $table->longText('post');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
